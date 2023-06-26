3cbb65f9f23f201354e551438ff24361f5b1e204
Denison Kolling <denisonkolling@gmail.com>
2023-06-26 14:09:51 -0300

    ex 04

--- exercicio4.js
+++ exercicio4.js
@@ -6,3 +6,26 @@
  * 0, 1, 1, 2, 3, 5, 8, 13, ...
  * Utiliza uma estrutura de repetição (while) para gerar a sequência.
  */
+
+var numero = 14;
+var i = 1;
+var fibonacci = "";
+
+let a = 0;
+let b = 1;
+let c = 0;
+
+fibonacci += a + " " + b
+
+do {
+
+  c = a + b;
+  fibonacci += " " + c;
+  a = b;
+  b = c;
+
+  i++;
+
+} while (i <= numero)
+
+  console.log(fibonacci)
