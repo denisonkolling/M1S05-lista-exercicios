9eea6e124adb94b19a599289445cd69a3f9360b7
Denison Kolling <denisonkolling@gmail.com>
2023-06-26 13:39:08 -0300

    ex 03

--- exercicio3.js
+++ exercicio3.js
@@ -5,3 +5,31 @@
  * é um número primo. Um número primo é aquele que é divisível apenas por 1 e por ele mesmo.
  * Utiliza uma estrutura de repetição (do while) para verificar as divisões.
  */
+
+var num = 7;
+var i = num;
+var numeroPrimo = 0;
+
+
+do {
+
+  if(num % i == 0){
+    numeroPrimo++
+  }
+
+  i--;
+
+} while (i > 0)
+
+if (numeroPrimo == 2){
+
+  console.log(num + " é primo")
+
+} else {
+  
+  console.log(num + " não é primo")
+  
+}
+
+
+
